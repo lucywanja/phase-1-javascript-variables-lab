538d7e4e46ae0fe052726e58cb91364643a17a4d
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-03-25 21:44:42 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+
+// Defining companyName to Scuber using const
+const companyName = 'Scuber';
+
+// declaring mostProftableneighborhood equal to Chelsea 
+let mostProfitableNeighborhood = 'Chelsea';
+
+// declaring companyCeo equal to susan smith and use let 
+let companyCeo = 'Susan Smith';
